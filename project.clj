b134c91b17006c3dc0e3356e539883f2d47d0983
Omar De Los Santos <omar.dlsg@gmail.com>
2018-09-18 16:29:24 -0700

    Configure new paths and install server dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,13 @@
   :url "https://github.com/omarnyte/Web-Tic-Tac-Toe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[info.cukes/cucumber-java "1.2.4"]
+                 [info.cukes/cucumber-junit "1.2.4"]
+                 [junit/junit "4.12"]
+                 [org.json/json "20180813"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot web-tic-tac-toe.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :java-source-paths ["http-server"])
--- src/web_tic_tac_toe/core.clj
+++ src/web_tic_tac_toe/core.clj
@@ -1,6 +1,33 @@
 (ns web-tic-tac-toe.core
+  (:require [web-tic-tac-toe.default-handler])
   (:gen-class))
+
+; (import Router Middleware Server)
+
+; (defn get-default-handler
+;   []
+;   default-handler)
+
+; (defn get-routes
+;   []
+;   (doto (new java.util.HashMap)))
+
+; (defn get-directory
+;   []
+;   (doto (new java.util.HashMap)))
+
+; (defn setUpRouter 
+;   []
+;   (Router. (get-default-handler) (get-routes) (get-directory)))
+
+; (defn startServer
+;   []
+;   (Server. 5000 (setUpRouter) (setUpMiddlewareChain))))
+
+; (defn -main
+;   []
+;   startServer)
 
 (defn -main
   []
-  (println "Welcome to Web Tic Tac Toe!"))
+  (println "Hello, web!"))
