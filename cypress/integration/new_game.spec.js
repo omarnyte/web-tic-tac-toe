034515fa03cf2b6f357ef563640c52d7ac9370e3
Omar De Los Santos <omarnyte@gmail.com>
2018-10-08 11:16:21 -0700

    Write end-to-end tests for new game
    Include tests for human-v-human, computer-v-human, and human-v-computer

--- cypress/integration/new_game.spec.js
+++ cypress/integration/new_game.spec.js
@@ -1,13 +1,14 @@
-describe('TTT new game', function() {
+describe('new TTT game', function() {
   beforeEach(function() {
-    cy.visit('http://localhost:8888/')
+    cy.visit('/');
   });
 
   context('no new game selected', function() {
     it('does not mark the board when clicking on space', function() {
-      cy.get('[data-idx=0]').then(($firstSpace) => {
-        expect($firstSpace.text()).to.eq('');
-      });
+      cy.get('[data-idx=0]')
+        .click()
+        .should('have.text', '')
+        .and('not.have.class', 'marked');
     });
   });
 
@@ -17,11 +18,67 @@ describe('TTT new game', function() {
     });
 
     it('marks X as the first mark', function() {
-      cy.get('[data-idx=0]').then(($firstSpace) => {
-        $firstSpace.click()
-                    .should('have.text', 'X');
-        // expect($firstSpace.text()).to.eq('X');
-      });    
+      cy.get('[data-idx=0]')
+        .click()
+        .should('have.text', 'X');
+    });
+  
+    it('marks O as the second mark', function() {
+      cy.get('[data-idx=0]').click();
+      cy.get('[data-idx=1]')
+        .click()
+        .should('have.text', 'O')
+        .and('have.class', 'marked'); 
+    })
+  });
+
+  context('new computer-v-human game selected', function() {
+    beforeEach(function() {
+      cy.get('#ai-v-human').click();
+    })
+
+    it('does not mark board with human mark before AI moves', function() {
+      cy.get('[data-idx=1]')
+        .click()
+        .should('not.have.text', 'O')
+    });
+    
+    it('makes AI move first', function() {
+      let numOfXMarks = 0;  
+
+      cy.wait(5000);
+      cy.get('.space')
+        .each($space => {
+          if ($space.text() === 'X') numOfXMarks += 1;
+        }).then(() => {
+          expect(numOfXMarks).to.eq(1);
+        });
+    });
+  });
+
+  context('new human-v-computer game selected', function() {
+    beforeEach(function() {
+      cy.get('#human-v-ai').click();
+    });
+
+    it('marks human move first', function() {
+      cy.get('[data-idx=0]')
+        .click()
+        .should('have.text', 'X')
+        .and('have.class', 'marked');
+    });
+
+    it('marks AI move after human move', function() {
+      let numOfOMarks = 0;
+      
+      cy.get('[data-idx=0]').click();
+      cy.wait(5000);
+      cy.get('.space')
+        .each($space => {
+          if ($space.text() === 'O') numOfOMarks += 1;
+        }).then(() => {
+          expect(numOfOMarks).to.eq(1);
+        });
     });
   });
 
