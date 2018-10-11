57dc843d03a79c217a10d4b472aea6ef3f72dfcc
Omar De Los Santos <omarnyte@gmail.com>
2018-10-11 09:23:23 -0700

    Write Mocha test for ai.js

---
+++ frontend/test/ai.test.js
@@ -0,0 +1,19 @@
+import * as ai from '../public/scripts/ai';
+
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('testing ai.js', function() {
+  context('isAI', function() {
+    it('return true when current player is AI', function() {
+      const playersState = { 
+        "currentPlayerMark": "O",
+        "O": "ai",
+        "X": "human"
+      }
+
+      assert.true(ai.isAi(playersState));
+    });
+  });
+  
+});
